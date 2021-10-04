ab180d87aa49e80d9c0ba1d97bab7fbf0e2d790a
Ratan Varghese <ratanv@protonmail.com>
2021-10-04 02:12:08 -0400

    Seperate utest and ptest steps, run make for theft

--- build.zig
+++ build.zig
@@ -5,6 +5,7 @@ pub fn build(b: *std.build.Builder) void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
+    //Library files
     const sharedLib = b.addSharedLibrary("mon13", "src/mon13.zig", b.version(0, 4, 1));
     sharedLib.setBuildMode(mode);
     sharedLib.install();
@@ -12,6 +13,27 @@ pub fn build(b: *std.build.Builder) void {
     const staticLib = b.addStaticLibrary("mon13", "src/mon13.zig");
     staticLib.setBuildMode(mode);
     staticLib.install();
+
+    //Unit tests
+    const mon13Pkg = std.build.Pkg{
+        .name = "mon13",
+        .path = "src/mon13.zig",
+    };
+
+    var unitTests = b.addTest("test/unit.zig");
+    unitTests.addPackage(mon13Pkg);
+    unitTests.setBuildMode(mode);
+
+    const testStep = b.step("utest", "Run unit tests");
+    testStep.dependOn(&unitTests.step);
+
+    //Theft tests
+    const submoduleInit = b.addSystemCommand(&[_][]const u8{ "git", "submodule", "init" });
+    const submoduleUpdate = b.addSystemCommand(&[_][]const u8{ "git", "submodule", "update" });
+    submoduleUpdate.step.dependOn(&submoduleInit.step);
+
+    const makeTheft = b.addSystemCommand(&[_][]const u8{ "make", "-C", "theft" });
+    submoduleUpdate.step.dependOn(&submoduleUpdate.step);
 
     const propertyTestExe = b.addExecutable("ptest", "test/ptest.c");
     propertyTestExe.linkLibC();
@@ -23,20 +45,10 @@ pub fn build(b: *std.build.Builder) void {
     propertyTestExe.addIncludeDir("test");
     propertyTestExe.addIncludeDir("theft/inc");
     propertyTestExe.setBuildMode(mode);
-    propertyTestExe.install();
+    propertyTestExe.step.dependOn(&makeTheft.step);
 
     const propertyTestRun = propertyTestExe.run();
 
-    const mon13Pkg = std.build.Pkg{
-        .name = "mon13",
-        .path = "src/mon13.zig",
-    };
-
-    var main_tests = b.addTest("test/unit.zig");
-    main_tests.addPackage(mon13Pkg);
-    main_tests.setBuildMode(mode);
-
-    const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&main_tests.step);
-    test_step.dependOn(&propertyTestRun.step);
+    const propertyTestStep = b.step("ptest", "Run property-based tests (POSIX only)");
+    propertyTestStep.dependOn(&propertyTestRun.step);
 }
